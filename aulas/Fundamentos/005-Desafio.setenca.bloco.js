6711861900f161422db42cb152f09837a138d66f
DiegoTegani <45742711+DiegoTegani@users.noreply.github.com>
2023-01-02 11:00:12 -0300

    +

---
+++ aulas/Fundamentos/005-Desafio.setenca.bloco.js
@@ -0,0 +1,20 @@
+{
+    // imprimindo os numeros impares //
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+   
+}
+
+
+{
+    // imprimindo os numeros pares //
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+  
+}
